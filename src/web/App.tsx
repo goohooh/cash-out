aac32dac283d0a4c040a32d422de429b8797f6eb
goohooh <goo_oo@naver.com>
2021-03-06 11:35:36 +0900

    render every data

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import data from "../data/data.json";
+import Calendar from "./component/Calendar";
 
 function App() {
   return (
     <div className="App">
+      <ul>
+        {data.map((d: any, i: number) => (<li key={i}>{d.name}</li>))}
+      </ul>
+      <Calendar />
     </div>
   );
 }
