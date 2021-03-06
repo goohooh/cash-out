3e5b9381bd33a4c0aefc861afa1d791c3bda29f1
goohooh <goo_oo@naver.com>
2021-03-06 22:35:44 +0900

    remove warns

--- src/web/component/CalendarCell.tsx
+++ src/web/component/CalendarCell.tsx
@@ -2,27 +2,29 @@ import Schedule from "../../entity/schedule";
 import ScheduleChip from "./ScheduleChip";
 
 interface Props {
-    isToday: boolean;
-    date: Date;
-    data?: Schedule[];
-    onClick?: () => void;
+  isToday: boolean;
+  date: Date;
+  data?: Schedule[];
+  onClick?: () => void;
 }
 
 const CalendarCell = ({
-    isToday,
-    date,
-    data,
-    onClick,
+  isToday,
+  date,
+  data,
+  onClick,
 }: Props) => {
-    return (
-        <div onClick={onClick}>
-            {/* <p className={isToday ? "today" : ""}>{date.getDate()}</p> */}
-            <p className={isToday ? "today" : ""}>{date.getDate()}</p>
-            {data?.map(d => {
-                return (<ScheduleChip title={d.title} subtitle={d.subtitle} />)
-            })}
-        </div>
-    );
+  return (
+    <div onClick={onClick}>
+      <p className={isToday ? "today" : ""}>{date.getDate()}</p>
+      {data?.map((d, i) => {
+        return (
+          <ScheduleChip key={`${d.key}-${i}`}
+                        title={d.title}
+                        subtitle={d.subtitle} />)
+      })}
+    </div>
+  );
 
 }
 
