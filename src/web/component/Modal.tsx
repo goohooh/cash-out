d983f0a96a57bc02a3f48f686e9ecba97f4c386e
goohooh <goo_oo@naver.com>
2021-03-07 12:27:48 +0900

    모달 스타일링

--- src/web/component/Modal.tsx
+++ src/web/component/Modal.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
-import styles from "./Modal.module.css";
 import Expenditure from "../../entity/expenditure";
+import styles from "./Modal.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface ModalProps {
     isShowing: boolean;
@@ -9,23 +10,41 @@ interface ModalProps {
 }
 
 const Modal: FC<ModalProps> = ({ isShowing, hide, data }) => {
+  const total: number = data
+    .map(exp => exp.amount)
+    .reduce((acc, cur) => (acc += cur), 0);
+
   return isShowing
     ? (
       <div className={styles.dimmed}>
         <div className={styles.modal}>
-          <div className="modal-header">
-            <h3>비용 출금 정보</h3>
-            <button onClick={hide}>X</button>
+          <div className="modal-header flex justify-between align-center">
+            <h3 className={styles.title}>비용 출금 정보</h3>
+            <button className={styles.close} onClick={hide}>
+              <FontAwesomeIcon icon="times" size="2x" />
+            </button>
           </div>
+
+          <div className={styles.total}>
+            <h4>총 출금</h4>
+            <strong>{total} 원</strong>
+          </div>
+
           <ul>
             {data.map((exp, i) => {
               return (
-                <li key={i}>{exp.name} {exp.amount}</li>
+                <li key={i}
+                    className={styles.item}>
+                  <span>{exp.name}</span>
+                  <span>
+                    {exp.amount} 원
+                    <FontAwesomeIcon icon="chevron-right" className="m-left-smallest" />
+                  </span>
+                </li>
               );
             })}
           </ul>
-          <div>{data.map(exp => exp.amount).reduce((acc, cur) => (acc += cur), 0)}원</div>
-          </div>
+        </div>
       </div>
     )
     : null;
