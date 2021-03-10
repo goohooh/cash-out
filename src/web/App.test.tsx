b5f6e86ff05552c55a195882e88668ac833823a5
goohooh <goo_oo@naver.com>
2021-03-10 21:59:15 +0900

    fix broken test

--- src/web/App.test.tsx
+++ src/web/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appTitle = screen.getByText(/장부/i);
+  await waitFor(() => {
+    expect(appTitle).toBeInTheDocument();
+  })
 });
